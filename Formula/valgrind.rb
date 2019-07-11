821bbde282dafa866ba43072625b923a02d917ee
Elio Ferri <eferri@uvic.ca>
2019-07-11 15:30:22 -0700

    Add valgrind

---
+++ Formula/valgrind.rb
@@ -0,0 +1,39 @@
+class Valgrind < Formula
+  desc "Dynamic analysis tools (memory, debug, profiling)"
+  homepage "http://www.valgrind.org/"
+  
+  head do
+    url "https://github.com/eferri/valgrind.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  # Valgrind needs vcpreload_core-*-darwin.so to have execute permissions.
+  # See #2150 for more information.
+  skip_clean "lib/valgrind"
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-only64bit
+      --build=amd64-darwin
+    ]
+    system "./autogen.sh" if build.head?
+
+    # Look for headers in the SDK on Xcode-only systems: https://bugs.kde.org/show_bug.cgi?id=295084
+    unless MacOS::CLT.installed?
+      inreplace "coregrind/Makefile.am", %r{(\s)(?=/usr/include/mach/)}, '\1'+MacOS.sdk_path.to_s
+    end
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/valgrind", "ls", "-l"
+  end
+end
